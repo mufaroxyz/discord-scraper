10939eb1e6c5be4fdebfabc6b66a1b05cb6986fd
mufaroxyz <mufaro.priv@gmail.com>
2025-04-25 08:14:12 +0000

    feat: improve error handling with neverthrow

--- src/test/build-cache.test.ts
+++ src/test/build-cache.test.ts
@@ -1,23 +1,37 @@
 import { expect, test } from "bun:test";
 import type { BuildMetadata } from "../util/types";
 import { BUILD_CACHE_TARGET_FILE, BuildCache, MetadataNotFoundError } from "../util/build-cache";
+import { ok, err } from "neverthrow";
 
-test("Correct latest build cache parsing", async () => {
+test("BuildCache.pull should return Ok when correct data is provided", async () => {
     const sampleConfig: BuildMetadata = {
         buildVersion: 1,
         hash: "hello world"
     }
 
     Bun.file(BUILD_CACHE_TARGET_FILE).write(JSON.stringify(sampleConfig));
-    expect(await BuildCache.pull()).toEqual(sampleConfig);
+    const result = await BuildCache.pull();
+    expect(result).toEqual(ok(sampleConfig));
+})
 
+test("BuildCache.pull should return Err when incorrect data is provided", async () => {
     const sampleConfig1 = {
         hello: "world"
     };
 
     Bun.file(BUILD_CACHE_TARGET_FILE).write(JSON.stringify(sampleConfig1));
-    expect(await BuildCache.pull()).toEqual(new MetadataNotFoundError());
+    const result = await BuildCache.pull();
+    expect(result.isErr()).toBe(true);
+    if (result.isErr()) {
+        expect(result.error).toBeInstanceOf(MetadataNotFoundError);
+    }
 
-    Bun.file(BUILD_CACHE_TARGET_FILE).write(JSON.stringify({}));
-    expect(await BuildCache.pull()).toEqual(new MetadataNotFoundError());
+    const sampleConfig2 = {};
+
+    Bun.file(BUILD_CACHE_TARGET_FILE).write(JSON.stringify(sampleConfig2));
+    const result2 = await BuildCache.pull();
+    expect(result2.isErr()).toBe(true);
+    if (result2.isErr()) {
+        expect(result2.error).toBeInstanceOf(MetadataNotFoundError);
+    }
 })
--- src/util/build-cache.ts
+++ src/util/build-cache.ts
@@ -1,4 +1,5 @@
 import type { BuildMetadata } from "./types";
+import { err, ok, type Result } from "neverthrow";
 
 export const BUILD_CACHE_TARGET_FILE = "latest_build.json"
 
@@ -7,16 +8,16 @@ export class MetadataNotFoundError extends Error {
 };
 
 export class BuildCache {
-    static async pull(): Promise<BuildMetadata | MetadataNotFoundError> {
+    static async pull(): Promise<Result<BuildMetadata, MetadataNotFoundError>> {
         const file = Bun.file(`./${BUILD_CACHE_TARGET_FILE}`);
         const contents = await file.json() as BuildMetadata;
 
-        if (Object.keys(contents).length == 0) return new MetadataNotFoundError();
+        if (Object.keys(contents).length == 0) return err(new MetadataNotFoundError());
 
         if ((!contents["buildVersion"] || !contents["hash"])) {
-            return new MetadataNotFoundError();
+            return err(new MetadataNotFoundError());
         };
 
-        return contents;
+        return ok(contents);
     }
 }
